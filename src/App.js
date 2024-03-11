922caf42e400367a31a36c308e8a9ff088b20193
joody <ser56421@gmail.com>
2024-03-11 21:18:11 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [data, setdata] = useState({})
+  useEffect(()=>{
+    const interv = setInterval(()=>{
+      fetch('https://api.openweathermap.org/data/2.5/forecast?q=yaroslavl&units=metric&appid=1f488e4442a49d96696206fd8b1d75bb')
+      .then(response => response.json())
+      .then(data => {
+        console.log(data.list[0])
+        setdata(data)
+      })
+    },60000)
+    return ()=>{clearInterval(interv)}
+  })
+    
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {Math.round(data.list[0].main.temp)}
     </div>
   );
 }
