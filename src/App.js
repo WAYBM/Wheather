15dede57c20a4662d0da39ab47ac4a24677c3faf
WAYBM <ser56421@gmail.com>
2024-03-15 14:07:14 +0300

    second version

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
-import { useState, useEffect } from 'react';
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
-  const [data, setdata] = useState({})
-  useEffect(()=>{
-    const interv = setInterval(()=>{
-      fetch('https://api.openweathermap.org/data/2.5/forecast?q=yaroslavl&units=metric&appid=1f488e4442a49d96696206fd8b1d75bb')
-      .then(response => response.json())
-      .then(data => {
-        console.log(data.list[0])
-        setdata(data)
-      })
-    },60000)
-    return ()=>{clearInterval(interv)}
-  })
-    
-  return (
-    <div className="App">
-      {Math.round(data.list[0].main.temp)}
-    </div>
-  );
+  const [data, setdata] = useState({});
+  const [visible, setvisible] = useState(false)
+  const fetchfunc = () => {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/forecast?q=yaroslavl&units=metric&appid=1f488e4442a49d96696206fd8b1d75bb"
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data.list[0]);
+        setdata(data);
+        setvisible(true)
+      });
+  };
+  useEffect(() => {
+    fetchfunc();
+    const interv = setTimeout(fetchfunc, 10000);
+    return () => {
+      clearTimeout(interv);
+    };
+  }, []);
+  if (visible) {
+    return (
+      <div className="App">
+        <div className="Weather">
+          <div className="temp">Температура: {Math.round(data.list[0].main.temp)}</div>
+          <div className="feels_like">Ощущается как: {Math.round(data.list[0].main.feels_like)}</div>
+          <div className="maxtemp">Максимальная температура: {Math.round(data.list[0].main.temp_max)}</div>
+          <div className="mintemp">Минимальная температура: {Math.round(data.list[0].main.temp_min)}</div>
+        </div>
+        <div></div>
+      </div>
+    );
+  }else{
+    return(
+      <div style={{margin:'auto'}}>Loading...</div>
+    );
+  }
 }
 
 export default App;
