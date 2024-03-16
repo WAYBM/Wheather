7b6bc85ab0cb5cb036a0f96344dc152bc05a3978
WAYBM <ser56421@gmail.com>
2024-03-16 18:12:02 +0300

    2 version

--- src/App.js
+++ src/App.js
@@ -1,43 +1,101 @@
 import "./App.css";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
+import { YMaps, Map } from "@pbe/react-yandex-maps";
 
 function App() {
   const [data, setdata] = useState({});
-  const [visible, setvisible] = useState(false)
-  const fetchfunc = () => {
-    fetch(
-      "https://api.openweathermap.org/data/2.5/forecast?q=yaroslavl&units=metric&appid=1f488e4442a49d96696206fd8b1d75bb"
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data.list[0]);
-        setdata(data);
-        setvisible(true)
-      });
+  const [visible, setvisible] = useState(false);
+  const [town, settown] = useState("yaroslavl");
+  const [errortext, seterror] = useState("");
+  const [icon, seticon] = useState();
+  const [url, seturl] = useState(`https://api.openweathermap.org/data/2.5/forecast?q=${town}&units=metric&appid=1f488e4442a49d96696206fd8b1d75bb`)
+  const inputref = useRef("Yaroslavl");
+  const refmap = useRef(null);
+  const search = async (typeofsearch) => {
+    if (inputref.current.value === "") {
+      return 0;
+    }
+    if (typeofsearch === 'input') {
+      seturl(`https://api.openweathermap.org/data/2.5/forecast?q=${town}&units=metric&appid=1f488e4442a49d96696206fd8b1d75bb`)
+    }
+    if (typeofsearch === "map") {
+      seturl(`https://api.openweathermap.org/data/2.5/forecast?lat=${refmap.current._sourceEvent.originalEvent.coords[0]}&lon=${refmap.current._sourceEvent.originalEvent.coords[1]}&units=metric&appid=1f488e4442a49d96696206fd8b1d75bb`)
+    }
+    settown(inputref.current.value);
+    let response = await fetch(url);
+    let datao = await response.json();
+    if (response.ok) {
+      setvisible(true);
+      setdata(datao);
+      seterror("");
+    } else {
+      seterror("Not Found 404");
+    }
   };
   useEffect(() => {
-    fetchfunc();
-    const interv = setTimeout(fetchfunc, 10000);
-    return () => {
-      clearTimeout(interv);
-    };
+    search();
   }, []);
   if (visible) {
     return (
       <div className="App">
+        <YMaps>
+          <div ref={refmap}>
+            <Map 
+              onClick={(e) => {
+                console.log(e._sourceEvent.originalEvent.coords);
+              }}
+              state={{
+                center: [data.city.coord.lat, data.city.coord.lon],
+                zoom: 11,
+              }}
+            />
+          </div>
+        </YMaps>
         <div className="Weather">
-          <div className="temp">Температура: {Math.round(data.list[0].main.temp)}</div>
-          <div className="feels_like">Ощущается как: {Math.round(data.list[0].main.feels_like)}</div>
-          <div className="maxtemp">Максимальная температура: {Math.round(data.list[0].main.temp_max)}</div>
-          <div className="mintemp">Минимальная температура: {Math.round(data.list[0].main.temp_min)}</div>
+          <div>
+            <input
+              className="searching"
+              ref={inputref}
+              type="search"
+              placeholder="Yaroslavl"
+            />
+            <button
+              onClick={() => {
+                search('input');
+              }}
+            >
+              SEARCH
+            </button>
+            <button
+              className="search_map"
+              onClick={()=>{
+                console.log(refmap.current.firstChild.firstChild.center);
+                // search("map");
+              }}
+            >
+              SEARCH MAP
+            </button>
+          </div>
+          <div>{errortext}</div>
+          {/* <button onClick={fetchmap()}>MAP</button> */}
+          <div className="temp">
+            Температура: {Math.round(data.list[0].main.temp)}°C
+          </div>
+          <div className="feels_like">
+            Ощущается как: {Math.round(data.list[0].main.feels_like)}°C
+          </div>
+          <div className="maxtemp">
+            Максимальная температура: {Math.round(data.list[0].main.temp_max)}°C
+          </div>
+          <div className="mintemp">
+            Минимальная температура: {Math.round(data.list[0].main.temp_min)}°C
+          </div>
+          <img className="icon" src={icon}></img>
         </div>
-        <div></div>
       </div>
     );
-  }else{
-    return(
-      <div style={{margin:'auto'}}>Loading...</div>
-    );
+  } else {
+    return <div style={{ margin: "auto" }}>Loading...</div>;
   }
 }
 
