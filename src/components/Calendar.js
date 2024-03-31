554b934c6b24b0977ea65874166081cf8399697c
Leonid <Lyunonin@yandex.ru>
2024-03-31 20:40:44 +0300

    version 4.005

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -2,28 +2,50 @@ import { useState, useEffect, useRef } from "react";
 export default function Calendar({dates}) {
     const calref = useRef('')
     const select = (e)=>{
-        e.id = ``
-    }
-    useEffect(()=>{
-        let cal = document.getElementsByClassName('.Calendar')
-        console.log(cal);
-        let mass = []
-        if (document.querySelectorAll('.dates').length<1) {
+        let dates = document.querySelectorAll('.dates')
+        if (e.className != 'Calendar') {
             for (let i = 0; i < dates.length; i++) {
-                if(mass[mass.length-1]!=dates[i].dt_txt.substring(8,10)){
-                    mass.push(dates[i].dt_txt.substring(8,10))
-                }
+                dates[i].style = 'border: 1px solid grey;'
+                
             }
-            for (let i = 0; i < mass.length; i++) {
-                console.log(mass[i]);
-                calref.current.insertAdjacentHTML('beforeEnd', `<div class="dates">${mass[i]}</div>` )
+            if (e.parentNode.className == 'dates') {
+                e.parentNode.style = 'border: 1px solid lime;'
+            }else{
+                e.style = 'border: 1px solid lime;'
             }
         }
-        console.log(mass);
+    }
+    const getMonthName = (MonthNum) =>{
+        let date = new Date()
+        date.setMonth(MonthNum - 1)
+        return date.toLocaleString('ru', {month:'long'})
+    }
+    useEffect(()=>{
+        let date = new Date();
+        let cal = document.getElementsByClassName('.Calendar')
+        let mass = []
+        let mass2 = []
+        let mass3 = []
+        if (document.querySelectorAll('.dates').length<1) {
+            for (let i = 0; i < dates.length; i++) {
+                if (mass2[mass2.length-1]!=dates[i].dt_txt.substring(8,10)) {
+                    mass2.push(dates[i].dt_txt.substring(8,10))
+                    const key = i
+                    const value = dates[i]
+                    mass.push(dates[i].dt_txt.substring(5,7))
+                    mass3.push(i)  
+                }
+            }
+            console.log(mass[0][0].dt_txt);
+            for (let i = 0; i < mass2.length; i++) {
+                calref.current.insertAdjacentHTML('beforeEnd', `<div class="dates" id=${mass2[i]}><p>${mass2[i]}</p><p>${getMonthName(mass[i])}</p></div>` )
+            }
+        }
+        console.log(date.getMonth());
     },[])
     return (
         <div ref={calref} className="Calendar" onClick={(e)=>{
-            console.log(e.target.id);
+            select(e.target)
         }}>
 
         </div>
--- src/components/Main.js
+++ src/components/Main.js
@@ -12,12 +12,32 @@ export const Main = () => {
   const [town, settown] = useState("Ярославль");
   const [errortext, seterror] = useState("");
   const [icon, seticon] = useState();
+  const [day, setday] = useState(0)
+  const [time, settime] = useState(null)
   const inputref = useRef("Yaroslavl");
+  const setting_day = (e) => {
+    let date = new Date();
+    let mass = []
+    for (let i = 0; i < data.list.length; i++) {
+      if (data.list[i].dt_txt.substring(8, 10) == Number(e.id)) {
+        mass.push({ num: i, time: data.list[i].dt_txt.substring(11, 13) })
+      }
+    }
+    if (mass[0].num != 0) {
+      setday(mass[4].num)
+      settime(`${mass[4].time}:00`)
+    } else {
+      setday(0)
+      settime(`${date.getHours()}:${date.getMinutes()}`)
+    }
+    setBGImg(BG(data.list[day].weather[0].main))
+    seticon(`https://openweathermap.org/img/wn/${data.list[day].weather[0].icon}@2x.png`) 
+    console.log(mass);
+  }
   const fetchSearch = (url) => {
     if (inputref.current.value == '') {
       return 0
     }
-    console.log(inputref.current.value);
     setBGImg('https://wallpaper.dog/large/20509187.jpg')
     fetch(url).then(res => {
       if (res.ok) {
@@ -27,9 +47,9 @@ export const Main = () => {
       }
     }).then(datao => {
       if (datao != false) {
-        setBGImg(BG(datao.list[0].weather[0].main))
+        setBGImg(BG(datao.list[day].weather[0].main))
         seticon(
-          `https://openweathermap.org/img/wn/${datao.list[0].weather[0].icon}@2x.png`
+          `https://openweathermap.org/img/wn/${datao.list[day].weather[0].icon}@2x.png`
         );
         setdata(datao)
         console.log(datao);
@@ -37,9 +57,19 @@ export const Main = () => {
       }
     }).catch((e) => console.log(e))
   }
+  
   useEffect(() => {
     fetchSearch(`https://api.openweathermap.org/data/2.5/forecast?q=${town}&units=metric&appid=1f488e4442a49d96696206fd8b1d75bb`);
   }, []);
+  // useEffect(()=>{
+  //   if (data.list[day].weather[0].main == undefined) {
+  //     console.log('cock');
+  //     return 0
+  //   }else{
+  //     setBGImg(BG(data.list[day].weather[0].main))
+  //     seticon(`https://openweathermap.org/img/wn/${data.list[day].weather[0].icon}@2x.png`) 
+  //   }
+  // },[day])
   if (visible) {
     return (
       <div className="App">
@@ -74,21 +104,32 @@ export const Main = () => {
           <div className="case_2">
             <div>{errortext}</div>
             <img className="icon" src={icon}></img>
-            <div className="temp">{Math.round(data.list[0].main.temp)}°C</div>
+            <div className="temp">{Math.round(data.list[day].main.temp)}°C</div>
           </div>
           <div className="case_3">
+            <div>{time}</div>
             <div className="feels_like">
-              По ощущению {Math.round(data.list[0].main.feels_like)}°C
+              По ощущению {Math.round(data.list[day].main.feels_like)}°C
             </div>
             <div className="wind">
               <p>
-                Ветер {Math.round(data.list[0].wind.speed)} м/с
+                Ветер {Math.round(data.list[day].wind.speed)} м/с
               </p>
               <img src={windIC} className="windIMG"></img>
             </div>
           </div>
         </div>
-        <Calendar dates={data.list}/>
+        <div style={{ width: '70%' }} className="helpjer" onClick={(e) => {
+          if (e.target.className === "dates" || e.target.parentNode.className === 'dates') {
+            if (e.target.parentNode.className === 'dates') {
+              setting_day(e.target.parentNode)
+            } else {
+              setting_day(e.target)
+            }
+          }
+        }}>
+          <Calendar dates={data.list} />
+        </div>
       </div>
     );
   } else {
